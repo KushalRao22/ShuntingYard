e077390531e1a14ed4433a0bbce809ec50e9830a
U-BSD\455644 <455644@SHS-10D9C0189.beaverton.k12.or.us>
2022-03-01 12:10:18 -0800

    Finished everything need to add comments

--- main.cpp
+++ main.cpp
@@ -10,26 +10,24 @@ struct Node{//Create a student struct
   struct Node *next;
 };
 
-Node* stackHead = NULL;
-Node* queueHead = NULL;
-Node* queueEnd = NULL;
-BinaryTreeNode* BTStackHead = NULL;
-BinaryTreeNode* treeHead = NULL;
-
-void printTree();
-void push(Node* newNode);
-void enqueue(Node* newNode);
-void printQueue();
-void pop(Node* &current);
-void dequeue(Node* &current);
-void printStack();
-char peep();
-void makeTree();
-void printBTStack();
-void BTPush(BinaryTreeNode* newNode1);
-void BTPop(Node* &current);
+void push(Node* newNode, Node* &stackHead);
+void enqueue(Node* newNode, Node* &queueHead, Node* &queueEnd);
+void pop(Node* &current, Node* &stackHead);
+void dequeue(Node* &current, Node* &queueHead, Node* &queueEnd);
+char peep(Node* &stackHead);
+void makeTree(Node* &queueHead, Node* &queueEnd, BinaryTreeNode* &treeHead, BinaryTreeNode* &BTStackHead);
+void BTPush(BinaryTreeNode* newNode1, BinaryTreeNode* &BTStackHead);
+void BTPop(Node* &current, BinaryTreeNode* &BTStackHead);
+void pre(BinaryTreeNode* current);
+void pos(BinaryTreeNode* current);
+void inf(BinaryTreeNode* current);
 
 int main(){
+  Node* stackHead = NULL;
+  Node* queueHead = NULL;
+  Node* queueEnd = NULL;
+  BinaryTreeNode* BTStackHead = NULL;
+  BinaryTreeNode* treeHead = NULL;
   char input[256];
   cin.get(input, 256);
   char infix[256];
@@ -44,109 +42,126 @@ int main(){
     Node* newNode = new Node;
     newNode->value = temp;
     if(temp == '+' || temp == '-' || temp == '*' || temp == '/' || temp == '^' || temp == '('){
-      push(newNode);
+      push(newNode, stackHead);
     }
     else if(temp == ')'){
       Node* nodeTemp;
       bool run = true;
       while(run){
-	if(peep() != '('){
-	  pop(nodeTemp);
-	  enqueue(nodeTemp);
+	if(peep(stackHead) != '('){
+	  pop(nodeTemp, stackHead);
+	  enqueue(nodeTemp, queueHead, queueEnd);
 	}
 	else{
-	  pop(nodeTemp);
+	  pop(nodeTemp, stackHead);
 	  run = false;
 	}
       }
     }
     else{
-      enqueue(newNode);
+      enqueue(newNode, queueHead, queueEnd);
     }
   }
   bool run = true;
   Node* nodeTemp;
   while(run){
     if(stackHead != NULL){
-      pop(nodeTemp);
-      enqueue(nodeTemp);
+      pop(nodeTemp, stackHead);
+      enqueue(nodeTemp, queueHead, queueEnd);
     }
     else{
       run = false;
     }
   }
-  printQueue();
-
-  makeTree();
-
-  printTree();
-  cout << "end" << endl;
+  makeTree(queueHead, queueEnd, treeHead, BTStackHead);
+  cout << "What do you want print? Infix/Postfix/Prefix" << endl;
+  cin.clear();
+  cin >> input;
+  if(strcmp(input, "pre") == 0){
+    pre(treeHead);
+  }
+  if(strcmp(input, "pos") == 0){
+    pos(treeHead);
+  }
+  if(strcmp(input, "inf") == 0){
+    inf(treeHead);
+  }
   return 0;
 }
 
-void printTree(){
-  bool run = true;
-  BinaryTreeNode* current = treeHead;
-  while(run){
-    cout << "Here" << endl;
-    cout << current->value << endl;
-    if(current->left  == NULL){
-      run = false;
-    }
-    else{
-      cout << "Left: " << current->getLeft()->value << "Right: " << current->getRight()->value << endl;
-      current = current->getRight();
-    }
 
-
+void pre(BinaryTreeNode* current){
+  cout << current->value;
+  if(current->left != NULL) {
+    pre(current->left);//Recurcive call
   }
-
-
+  if(current->right != NULL) {
+    pre(current->right);//Recurcive call
+  }
 }
 
-void push(Node* newNode){
+void pos(BinaryTreeNode* current){
+  if(current->left != NULL) {
+    pos(current->left);//Recurcive call
+  }
+  if (current->right != NULL) {
+    pos(current->right);//Recurcive call
+  }
+  cout << current->value;
+}
+
+void inf(BinaryTreeNode* current){
+  if(current->left != NULL) {
+    inf(current->left);//Recurcive call
+  }
+  cout << current->value;
+  if(current->right != NULL) {
+    inf(current->right);//Recurcive call
+  }
+}
+
+void push(Node* newNode, Node* &stackHead){
   newNode->next = stackHead;
   stackHead = newNode;
 }
 
-void pop(Node* &current){
+void pop(Node* &current, Node* &stackHead){
   current = stackHead;
   stackHead = stackHead->next;
 }
 
-
-char peep(){
+char peep(Node* &stackHead){
   return stackHead->value;
 }
 
-void BTpush(BinaryTreeNode* newNode1){
+void BTpush(BinaryTreeNode* &newNode1, BinaryTreeNode* &BTStackHead){
   newNode1->next = BTStackHead;
   BTStackHead = newNode1;
 }
 
-void BTPop(BinaryTreeNode* &current){
+void BTPop(BinaryTreeNode* &current, BinaryTreeNode* &BTStackHead){
   current = BTStackHead;
   BTStackHead = BTStackHead->next;
 }
 
-void makeTree(){
+void makeTree(Node* &queueHead, Node* &queueEnd, BinaryTreeNode* &treeHead, BinaryTreeNode* &BTStackHead){
   bool run = true;
   Node* current;
   while(run){
     if(queueHead->value != '+' && queueHead->value != '-' && queueHead->value != '*' && queueHead->value != '/' && queueHead->value != '^'){
-      dequeue(current);
+      dequeue(current, queueHead, queueEnd);
       BinaryTreeNode* BTcurrent = new BinaryTreeNode(current->value);
-      BTpush(BTcurrent);
+      BTpush(BTcurrent, BTStackHead);
     }
     else{
-      dequeue(current);
+      dequeue(current, queueHead, queueEnd);
       BinaryTreeNode* BTcurrent = new BinaryTreeNode(current->value);
       BinaryTreeNode* point;
-      BTPop(point);
+      BTPop(point, BTStackHead);
       BTcurrent->setRight(point);
-      BTPop(point);
+      BTPop(point, BTStackHead);
       BTcurrent->setLeft(point);
-      BTpush(BTcurrent);
+      BTpush(BTcurrent, BTStackHead);
       treeHead = BTcurrent;
     }
     if(queueHead == NULL){
@@ -155,12 +170,12 @@ void makeTree(){
   }
 }
 
-void dequeue(Node* &current){
+void dequeue(Node* &current, Node* &queueHead, Node* &queueEnd){
   current = queueHead;
   queueHead = queueHead->next;
 }
 
-void enqueue(Node* newNode){
+void enqueue(Node* newNode, Node* &queueHead, Node* &queueEnd){
   if(queueHead == NULL){
     newNode->prev = NULL;
     newNode->next = NULL;
@@ -173,54 +188,4 @@ void enqueue(Node* newNode){
     newNode->next = NULL;
     queueEnd = newNode;
   } 
-}
-
-void printBTStack(){
-  bool run = true;
-  BinaryTreeNode* current = BTStackHead;
-  if(current == NULL){
-    run = false;
-  }
-  while(run){
-    cout << current->value;
-    if(current->right != NULL){
-      current = current->right;
-    }
-    else{
-      run = false;
-    }
-  }
-  cout << endl;
-}
-
-void printQueue(){
-  bool run = true;
-  Node* current = queueHead;
-  while(run){
-    cout << current->value;
-    if(current->next != NULL){
-      current = current->next;
-    }
-    else{
-      run = false;
-    }
-  }
-  cout << endl;
-}
-
-void printStack(){
-  bool run = true;
-  if(stackHead == NULL){
-    run = false;
-  }
-  while(run){
-    cout << stackHead->value;
-    if(stackHead->next != NULL){
-      stackHead = stackHead->next;
-    }
-    else{
-      run = false;
-    }
-  }
-  cout << endl;
 }
