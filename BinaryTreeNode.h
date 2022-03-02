8f41f296d435b68119db4e28065c7fa8c3d9bee4
KushalRao22 <90669418+KushalRao22@users.noreply.github.com>
2022-03-02 11:59:35 -0800

    Finished comments

--- BinaryTreeNode.h
+++ BinaryTreeNode.h
@@ -1,16 +1,23 @@
+/*
+This is the .h file for the node that has a left and right pointer
+Last Modified: 3/2/22
+By: Kushal Rao
+*/
+//Imports
 #include <iostream>
 
 using namespace std;
 
 class BinaryTreeNode{
  public:
+ //Methods
   BinaryTreeNode(char valueIn);
   BinaryTreeNode* getLeft();
   void setLeft(BinaryTreeNode* newNode);
   BinaryTreeNode* getRight();
   void setRight(BinaryTreeNode* newNode);
   char getValue();
-  void setValue(char value);
+ //Variables
   char value;
   BinaryTreeNode* left;
   BinaryTreeNode* right;
