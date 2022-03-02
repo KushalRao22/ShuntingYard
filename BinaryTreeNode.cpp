192730b793dabb236eedb48c72ed1bc7c8436b5d
KushalRao22 <90669418+KushalRao22@users.noreply.github.com>
2022-03-02 11:58:08 -0800

    Finished comments

--- BinaryTreeNode.cpp
+++ BinaryTreeNode.cpp
@@ -1,34 +1,36 @@
+/*
+This is Node class with left and right pointers and holds a character 
+Last Modified: 3/2/22
+By: Kushal Ro
+*/
+//Imports
 #include <iostream>
 #include "BinaryTreeNode.h"
 
 using namespace std;
-
+//Create node with value and set pointers to null
 BinaryTreeNode::BinaryTreeNode(char valueIn){
   value = valueIn;
   left = NULL;
   right = NULL;
 }
-
+//Return left
 BinaryTreeNode* BinaryTreeNode::getLeft(){
   return left;
 }
-
+//Set left
 void BinaryTreeNode::setLeft(BinaryTreeNode* newNode){
   left = newNode;
 }
-
+//Return right
 BinaryTreeNode* BinaryTreeNode::getRight(){
   return right;
 }
-
+//Set right
 void BinaryTreeNode::setRight(BinaryTreeNode* newNode){
   right = newNode;
 }
-
+//Return value
 char BinaryTreeNode::getValue(){
   return value;
-}
-
-void BinaryTreeNode::setValue(char valueIn){
-  value = valueIn;
 }
